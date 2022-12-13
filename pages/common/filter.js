c7ca2763560061bf57f91698df36901ffb846a96
yoon.lee <pyhu26@gmail.com>
2022-12-13 11:23:03 +0900

    filter 요구사항 추가

--- pages/common/filter.js
+++ pages/common/filter.js
@@ -1,29 +1,63 @@
 import * as React from 'react';
-import axios from 'axios';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
-export default function filter(){
-
-    const [stateMenu, SetStateMenu] = React.useState(null);
-    var axios = require('axios');
-
-    var config = {
-    method: 'get',
-    url: 'https://a4678e66-520f-40ca-9b40-04715baefa64.mock.pstmn.io',
-    headers: { }
-    };
-
-    axios(config)
-    .then(function (response) {
-        console.log(JSON.stringify(response.data));
-        SetStateMenu(response.data);
-    })
-    .catch(function (error) {
-    console.log(error);
-    });
-
-    return(
-        <p>{
-            stateMenu
-        }</p>
+export default function filter() {
+    return (
+        <Container maxWidth="sx">
+            <Box component="main" sx={{ flexGrow: 1, p: 3, mt: 2 }}>
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Filters example
+                </Typography>
+                <ul>
+                    <li>
+                        tilter-textbox                        
+                    </li>
+                    <li>
+                        tilter-numberbox                        
+                        <ul> 
+                            <li> 문자 입력 불가 </li>
+                            <li> 소수점 자리수 설정 </li>                                
+                            <li> 천 단위 콤마 표시 </li>                                
+                        </ul>
+                    </li>
+                    <li>
+                        tilter-datebox
+                        <ul>
+                            <li> 일자만 선택 가능 </li>
+                            <li> 시간만 선택 가능 </li>
+                            <li> 알자 + 시간 선택 가능 </li>
+                            <li> 날짜 포맷 yyyy-MM-dd HH:mm:ss </li>
+                        </ul>
+                    </li>
+                    <li>
+                        tilter-radiogroup
+                        <ul>
+                            <li> name으로 표시, 선택 value는 id로 </li>                            
+                            <li> data로 동적 구성 </li>                            
+                        </ul>
+                    </li>
+                    <li>
+                        tilter-dropdown
+                        <ul>
+                            <li> multi header 기능 </li>
+                            <li> multi header 중 show header설정 </li>
+                            <li> multi check 기능 </li>
+                        </ul>
+                    </li>
+                    <li>
+                        filter-common
+                        <ul>
+                            <li> submit 시, 필수 조건 유효성 검증 </li>                            
+                            <li> 포맷 유효성 검증 (날짜) </li>                            
+                            <li> 필수 조건 표시 기능 </li>
+                            <li> 현재 value 반환 공통 함수 </li>
+                            <li> 현재 text 반환 공통 함수 </li>
+                        </ul>
+                    </li>                    
+                </ul>
+            </Box>
+        </Container>
     );
 }
--- pages/common/grid.js
+++ pages/common/grid.js
@@ -1,5 +1,29 @@
-export default function grid(){
-    return(
-        <h1>grid page</h1>
+import * as React from 'react';
+import axios from 'axios';
+
+export default function grid() {
+
+    const [stateMenu, SetStateMenu] = React.useState(null);
+    var axios = require('axios');
+
+    var config = {
+        method: 'get',
+        url: 'https://a4678e66-520f-40ca-9b40-04715baefa64.mock.pstmn.io',
+        headers: {}
+    };
+
+    axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+            SetStateMenu(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+    return (
+        <p>{
+            stateMenu
+        }</p>
     );
 }
